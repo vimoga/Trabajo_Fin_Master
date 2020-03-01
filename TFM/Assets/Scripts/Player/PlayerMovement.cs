ea50878fd129f137d0db6699c35ade18c6f65568
Unknown <neowar2@gmail.com>
2020-03-01 20:47:07 +0100

    Base test

---
+++ TFM/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// Clase encargada de implementar el movimento del jugador 
+/// mediante los elementos A* inplementados en Unity (NavMesh y NavMeshAgent).
+/// El personaje del jugador se movera donde este haya clicado del escenario.
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+    /// <summary>
+    /// GameObject del avatar del jugador
+    /// </summary>
+    private GameObject jugador;
+
+    /// <summary>
+    /// Agente del NavMesh del jugador
+    /// </summary>
+    private NavMeshAgent agente;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugador = GameObject.FindGameObjectWithTag("Player");
+        agente = jugador.GetComponent<NavMeshAgent>();
+    }
+
+
+    void Clicked()
+    {
+
+        //Obtencion del punto donde a clickado el jugador
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit = new RaycastHit();
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            //indicamos al agente que su destino es el punto marcado
+            agente.destination = hit.point;
+        }
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //detección del input del jugador
+        if (Input.GetMouseButtonDown(0))
+        {
+            Clicked();
+        }
+    }
+}
---
+++ TFM/Assets/Scripts/Structures/AA_Rotation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Modificacion de rotor.cs para laa estructura del arma antiaerea 
+/// </summary>
+public class AA_Rotation : MonoBehaviour
+{
+    Rigidbody rBody;
+    public float power;
+
+    /// <summary>
+    /// Specify the verse of the rotation
+    /// <para> Set this in the editor
+    /// </summary>
+    public bool counterclockwise = false;
+
+    /// <summary>
+    /// Specify is the rotors are animated or static. Just a visual effect
+    /// <para> Set this in the editor
+    /// </summary>
+    public bool animationActivated = false;
+
+    /// <summary>
+    /// Function called before of the first update
+    /// </summary>
+    void Start()
+    {
+        Transform t = this.transform;
+        while (t.parent != null && t.tag != "Player") t = t.parent;
+        rBody = t.GetComponent<Rigidbody>();
+    }
+
+    /// <summary>
+    /// Sets the rotating power of the rotor
+    /// </summary>
+    /// <param name="intensity"> The rotating power of the rotor </param>
+    public void setPower(float intensity) { power = intensity; }
+
+    /// <summary>
+    /// Gets the rotating power of the rotor
+    /// </summary>
+    /// <returns>the actual rotating power of the rotor</returns>
+    public float getPower() { return power; }
+
+    /// <summary>
+    /// Function called once per frame
+    /// </summary>
+    void Update() { if (animationActivated) transform.Rotate(0, power * 70 * Time.deltaTime * (counterclockwise ? -1 : 1), 0); }
+}
