41a3fbf133b3f1173aff99bb2b2a5868641d5629
Unknown <neowar2@gmail.com>
2020-03-02 20:12:32 +0100

    drone test

--- TFM/Assets/ImportedAssets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
+++ TFM/Assets/ImportedAssets/FREE GatlingGun/Turrets/Gatling Gun/Scripts/GatlingGun.cs
@@ -47,7 +47,7 @@ public class GatlingGun : MonoBehaviour
     // Detect an Enemy, aim and fire
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag == "Player")
         {
             go_target = other.transform;
             canFire = true;
@@ -57,7 +57,7 @@ public class GatlingGun : MonoBehaviour
     // Stop firing
     void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Enemy")
+        if (other.gameObject.tag == "Player")
         {
             canFire = false;
         }
