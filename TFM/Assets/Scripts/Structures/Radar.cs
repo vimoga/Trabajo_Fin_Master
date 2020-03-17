5c3b7ad17f947e06d613aac8b884384119d0c8dc
Unknown <neowar2@gmail.com>
2020-03-17 17:07:29 +0100

    Radar structure

---
+++ TFM/Assets/Scripts/Structures/Radar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour, StructuresInterfaces
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Attack()
+    {
+        Debug.Log("Eadars are unable to attack");
+    }
+
+    public void SetCaptured(bool isCaptured)
+    {
+        GetComponent<BasicStructure>().isCaptured = isCaptured;
+    }
+
+    public bool GetCaptured()
+    {
+        return GetComponent<BasicStructure>().isCaptured;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
