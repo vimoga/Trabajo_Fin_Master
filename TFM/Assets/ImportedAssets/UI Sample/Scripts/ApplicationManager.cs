e6d53a659862e100caf98747ef2c920c8a046326
Unknown <neowar2@gmail.com>
2020-03-24 21:51:37 +0100

    added start, pause and end menu

---
+++ TFM/Assets/ImportedAssets/UI Sample/Scripts/ApplicationManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour {
+	  
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Demo");
+    }
+
+    public void Restart()
+    {
+        if(SceneManager.GetActiveScene().name.Equals("Demo")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene("Demo");
+        }
+    }
+
+    public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("Demo"))
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                GameObject pauseMenu = GameObject.Find("Canvas");
+                if (pauseMenu) {
+                    if (pauseMenu.activeSelf)
+                    {
+                        pauseMenu.SetActive(false);
+                    }
+                    else {
+                        pauseMenu.SetActive(false);
+                    }
+                }                
+            }
+        } 
+    }
+}
